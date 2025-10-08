683244bd7cdf40e01aadacc66935a2dd5efa5c45
Oliver Deng <oliver.t.deng@gmail.com>
2025-10-08 13:28:16 -0400

    Initial uv stuff

---
+++ main.py
@@ -0,0 +1,15 @@
+from pprint import pprint
+
+
+def main():
+    data = {
+        "name": "Alice",
+        "age": 30,
+        "city": "New York",
+        "hobbies": ["reading", "traveling", "swimming"],
+    }
+    pprint(data)
+
+
+if __name__ == "__main__":
+    main()
