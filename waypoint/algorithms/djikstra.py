b4791f538ffa45d04be747b27cde6a96beaa5c30
Oliver Deng <oliver.t.deng@gmail.com>
2025-10-22 13:25:23 -0400

    Another quick test

--- waypoint/algorithms/djikstra.py
+++ waypoint/algorithms/djikstra.py
@@ -4,5 +4,4 @@ from ..path import Path
 def djikstra(
     graph: dict[int, list[tuple[int, float]]], start: int, destination: int
 ) -> Path:
-    ...
     raise NotImplementedError("Djikstra's algorithm is not yet implemented.")
