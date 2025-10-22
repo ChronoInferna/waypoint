2818b96fdbf06669e5b94e125379ecf29b236d5f
Oliver Deng <oliver.t.deng@gmail.com>
2025-10-22 13:00:26 -0400

    test: add fail details

--- tests/test_djikstra.py
+++ tests/test_djikstra.py
@@ -3,7 +3,7 @@ import pytest
 from waypoint.algorithms.djikstra import djikstra
 
 
-@pytest.mark.xfail
+@pytest.mark.xfail(reason="Djikstra algorithm not yet implemented")
 def test_djikstra():
     graph = {
         100: {"200": 1, "300": 4},
@@ -24,3 +24,22 @@ def test_djikstra():
     assert (
         res.distance == expected_cost
     ), f"Expected cost {expected_cost}, but got {res.distance}"
+
+
+@pytest.mark.xfail(reason="Djikstra algorithm not yet implemented")
+def test_djikstra_no_path():
+    graph = {
+        100: {"200": 1},
+        200: {},
+        300: {"400": 1},
+        400: {},
+    }
+    start = 100
+    end = 400
+
+    res = djikstra(graph, start, end)
+
+    assert res.flights is None, f"Expected no path, but got {res.flights}"
+    assert res.distance == float(
+        "inf"
+    ), f"Expected infinite cost, but got {res.distance}"
