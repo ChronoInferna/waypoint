bb7628a78bd6e8d6b21b02a23b928913912a629a
Oliver Deng <32346067+ChronoInferna@users.noreply.github.com>
2025-10-27 15:05:30 -0400

    feat: add a* algorithm (#6)

---
+++ waypoint/algorithms/a_star.py
@@ -0,0 +1,7 @@
+from ..path import Path
+
+
+def a_star(
+    graph: dict[int, list[tuple[int, float]]], start: int, destination: int
+) -> Path:
+    raise NotImplementedError("The A* algorithm is not yet implemented.")
