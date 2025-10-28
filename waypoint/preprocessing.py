1d3b8bb7c19953f644a9f218759802fe3e577d3b
Oliver Deng <oliver.t.deng@gmail.com>
2025-10-28 17:24:04 -0400

    test: fix tests

--- waypoint/preprocessing.py
+++ waypoint/preprocessing.py
@@ -8,16 +8,16 @@ def file_to_graph(file_path) -> dict[int, list[tuple[int, float]]]:
         reader = csv.DictReader(file)
         for row in reader:
             origin, dest, time = (
-                int(row["ORIGIN_AIRPORT_ID"]),
-                int(row["DEST_AIRPORT_ID"]),
-                float(row["ACTUAL_ELAPSED_TIME"]),
+                row["ORIGIN_AIRPORT_ID"],
+                row["DEST_AIRPORT_ID"],
+                row["ACTUAL_ELAPSED_TIME"],
             )
             if not origin or not dest or not time:
                 continue
             if origin not in graph:
                 graph[origin] = {}
             # TODO take lowest time if multiple flights exist between two airports
-            graph[origin][dest] = time
+            graph[origin][dest] = float(time)
     return graph
 
 
@@ -27,7 +27,7 @@ def file_to_airports(file_path) -> dict[int, str]:
     with open(file_path) as file:
         reader = csv.DictReader(file)
         for row in reader:
-            airport_id, airport_name = int(row["AIRPORT_ID"]), row["AIRPORT_NAME"]
+            airport_id, airport_name = row["AIRPORT_ID"], row["AIRPORT_NAME"]
             if airport_id and airport_name:
                 airports[airport_id] = airport_name
     return airports
