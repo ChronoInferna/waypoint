33c3ac0bfaaef6559408a12de6c9dbfab37978d4
Oliver Deng <oliver.t.deng@gmail.com>
2025-10-28 17:17:20 -0400

    fix: graph is a dict of dicts now

--- waypoint/preprocessing.py
+++ waypoint/preprocessing.py
@@ -8,15 +8,16 @@ def file_to_graph(file_path) -> dict[int, list[tuple[int, float]]]:
         reader = csv.DictReader(file)
         for row in reader:
             origin, dest, time = (
-                row["ORIGIN_AIRPORT_ID"],
-                row["DEST_AIRPORT_ID"],
-                row["ACTUAL_ELAPSED_TIME"],
+                int(row["ORIGIN_AIRPORT_ID"]),
+                int(row["DEST_AIRPORT_ID"]),
+                float(row["ACTUAL_ELAPSED_TIME"]),
             )
             if not origin or not dest or not time:
                 continue
             if origin not in graph:
-                graph[origin] = []
-            graph[origin].append((dest, time))
+                graph[origin] = {}
+            # TODO take lowest time if multiple flights exist between two airports
+            graph[origin][dest] = time
     return graph
 
 
@@ -26,7 +27,7 @@ def file_to_airports(file_path) -> dict[int, str]:
     with open(file_path) as file:
         reader = csv.DictReader(file)
         for row in reader:
-            airport_id, airport_name = row["AIRPORT_ID"], row["AIRPORT_NAME"]
+            airport_id, airport_name = int(row["AIRPORT_ID"]), row["AIRPORT_NAME"]
             if airport_id and airport_name:
                 airports[airport_id] = airport_name
     return airports
