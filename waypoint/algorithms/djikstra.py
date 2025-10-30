1ddcd9e1e76279c5a317d592c8720e7dd6264d07
mrw4211 <mrw4211@gmail.com>
2025-10-30 18:15:36 -0400

    Updated djikstra

--- waypoint/algorithms/djikstra.py
+++ waypoint/algorithms/djikstra.py
@@ -5,15 +5,17 @@ import heapq
 def djikstra(graph: dict[int, dict[int, float]], start: int, destination: int) -> Path:
 
     unvisited = {n: float("inf") for n in graph}  # Dict of unvisited nodes, all with infinite weight
+    if start not in graph.keys():
+        raise KeyError
     unvisited[start] = 0  # Start node given weight of 0
     visited = {}
     previous = {}
 
     while unvisited:
-        minimum = min(unvisited, key=unvisited.get())  # Start from lowest weight
+        minimum = min(unvisited, key=unvisited.get)  # Start from lowest weight
         current_distance = unvisited[minimum]
 
-        for neighbor, weight in graph.get(minimum, []).items():  # Start from lowest weight
+        for neighbor, weight in graph.get(minimum, {}).items():  # Start from lowest weight
             if neighbor in visited:
                 continue
 
@@ -28,6 +30,8 @@ def djikstra(graph: dict[int, dict[int, float]], start: int, destination: int) -
     # Path starts at destination and iterates through predecessors
     path = [destination]
     while path[-1] != start:
+        if(path[-1] not in previous):
+            return Path(flights=None, distance=float("inf"))
         path.append(previous[path[-1]])
     path.reverse()  # Reverse list so path is start location to end location
 
